07c84c05aa1b488c92fc754fa0cc9c8b09149679
pathpi23 <pa.th.pi.23@gmail.com>
2021-03-02 23:03:40 +0700

    edit main image

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ import document as Doc
 import pdf2Image as P2i
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+# func rotate
+TEXT_MIN_WIDTH = 15      # min reduced px width of detected text contour
+TEXT_MIN_HEIGHT = 2      # min reduced px height of detected text contour
+TEXT_MIN_ASPECT = 1.5    # filter out text contours below this w/h ratio
+TEXT_MAX_THICKNESS = 10  # max reduced px thickness of detected text contour
+TEXT_MAX_HEIGHT = 100 
+########################
+
 def histogram(gray):
     hist = cv2.calcHist([gray],[0],None,[256],[0,256])
     plt.plot(hist, color='k')
@@ -85,11 +93,12 @@ def tesseractOcr(picture):
 
 def repairImage(picture):
     kernalRepair = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
-    return cv2.morphologyEx(picture, cv2.MORPH_ERODE, kernalRepair, iterations=1)
+    return cv2.morphologyEx(picture, cv2.MORPH_HITMISS, kernalRepair, iterations=1)
 
 def main():
-    # path = P2i.convertPdftoJpg('./documents-pdf/kati56.pdf','kati56')
-    image = cv2.imread('./documents-image/kati54/page22.jpg')
+    # path = P2i.convertPdftoJpg('./documents-pdf/kati56.pdf','kati56')\
+    BLUE = [255,255,255]
+    image = cv2.imread('./documents-image/kati50/page22.jpg')
     imageText = removeBG(image)    
     kernalDilate = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(13,5))
     dilate = cv2.morphologyEx(imageText, cv2.MORPH_CLOSE, kernalDilate, iterations=3)
@@ -99,24 +108,34 @@ def main():
         boundaryBox.append(cv2.boundingRect(cnt))
 
     sortCnts = sortTextOCR(boundaryBox, image.shape[1])
-    resize('before', dilate)
+    # resize('before', dilate)
+
     imageText = cv2.bitwise_not(imageText)
     imageRepair = repairImage(imageText)
+
     arrayPicture = []
     arrayText = []
     arrayText2 = []
 
     for inx, box in enumerate(sortCnts):
         x,y,w,h = box
-        repairOCR = imageRepair[y:y+h, x:x+w].copy()
-        cropToOCR = imageText[y:y+h, x:x+w].copy()
-        image = cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0),2)        
-        arrayPicture.append(cropToOCR)
-        arrayText.append(tesseractOcr(cropToOCR))
-        arrayText2.append(tesseractOcr(repairOCR))
-        cv2.destroyAllWindows()
+        if h < TEXT_MIN_HEIGHT or w < TEXT_MIN_WIDTH: 
+            print("paste")
+        else:
+            repairOCR = imageRepair[y:y+h, x:x+w].copy()
+            cropToOCR = imageText[y:y+h, x:x+w].copy()
+                
+            image = cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0),2)      
+            # cropToOCR= cv2.copyMakeBorder(cropToOCR.copy(),5,5,5,5,cv2.BORDER_CONSTANT,value=BLUE)
+            # repairOCR= cv2.copyMakeBorder(repairOCR.copy(),5,5,5,5,cv2.BORDER_CONSTANT,value=BLUE)
 
-    Doc.addReportDoc(arrayPicture, arrayText,arrayText2,'./docRep4.docx')
+            arrayPicture.append(cropToOCR)
+            arrayText.append(tesseractOcr(cropToOCR))
+            arrayText2.append(tesseractOcr(repairOCR))
+            cv2.destroyAllWindows()
+    resize('sd', image)
+
+    Doc.addReportDoc(arrayPicture, arrayText,arrayText2,'./repch.docx')
 
 if __name__ == '__main__':
     try:
